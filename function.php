24def2aadcecd93010037b6e2e3fe738314152d0
SivaNurI <sivanurislamii@gmail.com>
2019-09-08 18:02:08 +0700

    function

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Fungsi/Function adalah sekumpulan
+   intruksi yang dibungkus dalam sebuah blok.
+   Fungsi dapat digunakan ulang tanpa harus
+   menulis ulang instruksi didalamnya.
+*/
+
+
+//Membuat Function Dengan Nama "Hallo"
+function Hallo()
+{
+	echo "Hallo Nama Saya Siva<br>";
+}
+
+//Memanggil Function "Hallo"
+Hallo();
